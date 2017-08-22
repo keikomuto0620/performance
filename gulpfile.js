fab71a19d1450ce85cc421f3a91751fa45d8e2fc
steelydylan <ess_president@me.com>
2017-08-23 00:34:31 +0900

    finish

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var rename = require('gulp-rename');
+var autoPrefixer = require('gulp-autoprefixer');
+//if node version is lower than v.0.1.2
+require('es6-promise').polyfill();
+var cssComb = require('gulp-csscomb');
+var cmq = require('gulp-merge-media-queries');
+var cleanCss = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var minifyHtml = require('gulp-minify-html');
+var inlineimg = require('gulp-inline-image-html');
+var inlineCss = require('gulp-inline-css');
+
+gulp.task('html',function(){
+    gulp.src(['src/index.html'])
+        .pipe(plumber({
+            handleError: function (err) {
+                console.log(err);
+                this.emit('end');
+            }
+        }))
+        .pipe(inlineCss())
+        .pipe(inlineimg('./'))
+        .pipe(minifyHtml())
+        .pipe(gulp.dest('./'))
+});
+
+gulp.task('default',function(){
+    gulp.watch('src/index.html',['html']);
+});
